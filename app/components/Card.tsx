d643743502f142bde429d794180f2442c233ad63
Maor <maoridi16@gmail.com>
2024-02-28 22:02:54 +0200

    added card component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,37 +1,55 @@
 "use client";
 import React from "react";
 import styles from "@/app/styles/Card.module.css";
-import { redirect } from "next/navigation";
+import { useState } from "react";
 type props = {
   title: string;
   description: string;
   tags: string[];
   date: string;
+  linkTo: string;
 };
 
 const Card = (props: props) => {
-  const handleRedirect = () => {
-    window.open("https://20.120.7.5/");
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const handleShowPopUp = () => {
+    setShowPopUp(!showPopUp);
   };
   return (
-    <div className={styles.outer} onClick={handleRedirect}>
-      <p className={styles.date}>{props.date}</p>
-      <div className={styles.inner}>
-        <a className={styles.title}>
-          {props.title} · <i className="fa-thin fa-arrow-up-right"></i>
-        </a>
-        <p>{props.description}</p>
-        <div className={styles.tags}>
-          {props.tags.map(
-            (tag, index): React.ReactNode => (
-              <p key={index} className={styles.tag}>
-                {tag}
-              </p>
-            )
-          )}
+    <>
+      {showPopUp ? (
+        <>
+          <div className={`${styles.popUp} ${showPopUp && styles.hide}`}>
+            <i
+              className={`fa-regular fa-x ${styles.xButton}`}
+              onClick={handleShowPopUp}
+            ></i>
+            <iframe src={props.linkTo} height="800" width="1080"></iframe>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+      <div className={styles.outer} onClick={handleShowPopUp}>
+        <p className={styles.date}>{props.date}</p>
+        <div className={styles.inner}>
+          <a className={styles.title}>
+            {props.title} · <i className="fa-thin fa-arrow-up-right"></i>
+          </a>
+          <p>{props.description}</p>
+          <div className={styles.tags}>
+            {props.tags.map(
+              (tag, index): React.ReactNode => (
+                <p key={index} className={styles.tag}>
+                  {tag}
+                </p>
+              )
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/styles/HomePage.module.css";
 import LeftNav from "@/app/components/LeftNav";
 import Card from "@/app/components/Card";
 const HomePage = () => {
-  let ghostSquawk = `
+  const kovachTech = `
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
 molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
 numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
@@ -12,22 +12,17 @@ obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
 nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
 tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
 quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-doloremque. Quaerat provident commodi consectetur veniam similique ad 
-earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
-fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
-suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
-totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
-quasi aliquam eligendi, placeat qui corporis!
-  
   `;
-
+  const ghostSquawk = `
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+  `;
   return (
     <>
       <link
@@ -48,22 +43,28 @@ quasi aliquam eligendi, placeat qui corporis!
             <div className={styles.links}>
               <ul>
                 <li>
-                  <a href="">
+                  <a href="https://github.com/MaorIdi" target="_blank">
                     <i className="fa-brands fa-github"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a
+                    href="https://www.linkedin.com/in/maor-idi-99326b215/"
+                    target="_blank"
+                  >
                     <i className="fa-brands fa-linkedin"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a
+                    href="https://www.instagram.com/maoridi_sw/"
+                    target="_blank"
+                  >
                     <i className="fa-brands fa-instagram"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="" target="_blank">
                     <i className="fa-brands fa-x-twitter"></i>
                   </a>
                 </li>
@@ -73,37 +74,87 @@ quasi aliquam eligendi, placeat qui corporis!
         </div>
         <div className={styles.right}>
           <section id="first">
+            <h1>About me</h1>
             <div className={styles.description}>
-              Back in 2012, I decided to try my hand at creating custom Tumblr
-              themes and tumbled head first into the rabbit hole of coding and
-              web development. Fast-forward to today, and I’ve had the privilege
-              of building software for an <span>advertising agency</span>, a
-              start-up, a student-led design studio, and a huge corporation. My
-              main focus these days is building products and leading projects
-              for our clients at Upstatement. I most enjoy building software in
-              the sweet spot where design and engineering meet — things that
-              look good but are also built well under the hood. In my free time,
-              I've also released an online video course that covers everything
-              you need to know to build a web app with the Spotify API. When I’m
-              not at the computer, I’m usually rock climbing, reading, hanging
-              out with my wife and two cats, or running around Hyrule searching
-              for Korok seeds K o r o k s e e d s .
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, <span>molestiae</span> quas vel sint commodi repudiandae
+              consequuntur voluptatum laborum numquam blanditiis harum quisquam
+              eius sed odit fugiat iusto fuga praesentium optio, eaque rerum!
+              Provident similique accusantium nemo autem. Veritatis obcaecati
+              tenetur iure eius earum ut molestias architecto voluptate aliquam
+              nihil, <span>eveniet aliquid culpa officia aut!</span> Impedit sit
+              sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis
+              quas aliquid. Reprehenderit, quia. Quo neque error repudiandae
+              fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt
+              excepturi expedita sint? Sed quibusdam recusandae alias error
+              harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
+              dolorem! Officiis iure rerum voluptates a cumque velit quibusdam
+              sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+              fugiat, temporibus enim commodi iusto libero magni deleniti quod
+              quam consequuntur! Commodi minima excepturi repudiandae velit hic
+              maxime doloremque. Quaerat provident commodi consectetur veniam
+              similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+              pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+              excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+              Voluptatem quaerat non architecto ab laudantium modi minima sunt
+              esse temporibus sint culpa, recusandae aliquam numquam totam
+              ratione voluptas quod exercitationem fuga. Possimus quis earum
+              veniam quasi aliquam eligendi, placeat qui corporis!
             </div>
           </section>
           <br />
           <section id="second">
             <div className={styles.experience}>
-              <h1>Card Components</h1>
+              <h1>Experience</h1>
               <Card
                 title="Ghost Squawk"
                 description={ghostSquawk}
-                tags={["CSS", "Javascript", "Python", "Project Manager", "PHP"]}
+                tags={[
+                  "CSS",
+                  "Javascript",
+                  "Python",
+                  "PHP",
+                  "Project Manager",
+                  "Server Manager",
+                  "MySQL",
+                ]}
                 date="2021 — 2022"
+                linkTo="http://localhost/GhostSquawkV2/"
+              />
+              <Card
+                title="Kovach Technologies"
+                description={kovachTech}
+                tags={[
+                  "CSS",
+                  "Javascript",
+                  "Python",
+                  "Project Manager",
+                  "Server Manager",
+                  "PHP",
+                  "MySQL",
+                ]}
+                date="July — Dec 2022"
+                linkTo="http://localhost/DEFT-Analytics/"
+              />
+              <Card
+                title="Mycri Services"
+                description={kovachTech}
+                tags={[
+                  "CSS",
+                  "Javascript",
+                  "Python",
+                  "Project Manager",
+                  "Server Manager",
+                  "PHP",
+                  "MySQL",
+                ]}
+                date="June — Sep 2023"
+                linkTo="http://localhost/Mycri-Website-main/"
               />
             </div>
           </section>
           <section id="third">
-            <h1>Third component</h1>
+            <h1>Projects</h1>
           </section>
         </div>
       </div>
