1745bf2a90347ec7c351c08185332547c40c9af3
Maor <60894008+MaorIdi@users.noreply.github.com>
2024-03-09 21:07:39 +0200

    pdf window.open

--- components/TechButton.tsx
+++ components/TechButton.tsx
@@ -8,10 +8,18 @@ type props = {
 
 const TechButton = (props: props) => {
   const [clicked, setClicked] = useState(false);
+  const openPDF = () => {
+    window.open("resume.pdf", "_blank");
+  };
 
   return (
     <>
-      <a href="/resume" target="_blank" className={styles.buttonA}>
+      <a
+        href="/resume"
+        target="_blank"
+        className={styles.buttonA}
+        onClick={openPDF}
+      >
         <button className={styles.button} onClick={() => setClicked(!clicked)}>
           <span>
             {props.title} ·{" "}
