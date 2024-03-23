fdb8b5e9fe9a909fd9efb55c211935f58997955e
Maor <maoridi16@gmail.com>
2024-03-23 23:49:07 +0200

    btctracker

---
+++ app/BitcoinTracker/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./page.module.css";
+const BitcoinTracker = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.showcase}>Bitcoin tracker</div>
+    </div>
+  );
+};
+
+export default BitcoinTracker;
